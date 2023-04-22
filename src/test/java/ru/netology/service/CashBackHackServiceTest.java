3cfcc43a575bb6e23d4763ad43b5253ffa74719b
Oleg <rbghero@gmail.com>
2023-04-22 14:52:17 +0300

    Tests added

---
+++ src/test/java/ru/netology/service/CashBackHackServiceTest.java
@@ -0,0 +1,35 @@
+package ru.netology.service;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class CashBackHackServiceTest {
+    @Test
+    public void remain() {
+        CashBackHackService service = new CashBackHackService();
+
+        int expected = 1;
+        int actual = service.remain(999);
+
+        Assert.assertEquals(actual, expected);
+    }
+    @Test
+    public void totalAmount() {
+        CashBackHackService service = new CashBackHackService();
+
+        int expected = 0;
+        int actual = service.remain(1000);
+
+        Assert.assertEquals(actual, expected);
+    }
+
+    @Test
+    public void amountIsLess() {
+        CashBackHackService service = new CashBackHackService();
+
+        int expected = 999;
+        int actual = service.remain(1001);
+
+        Assert.assertEquals(actual, expected);
+    }
+}
